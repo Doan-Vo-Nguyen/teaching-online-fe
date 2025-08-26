04dfe20f5b172815f6c0bca3e7651d26ca0570ae
Doan Vo Nguyen <wwwdoanvonguyen@gmail.com>
2025-08-26 08:47:12 +0700

    Initial commit in dev

--- src/utils/customAxios.js
+++ src/utils/customAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://localhost:10000",
+  baseURL: process.env.API_SERVICE_PRODUCT_URL,
   timeout: 10000,
   withCredentials: true,
 });
